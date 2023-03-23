598aa27fffd3134aabdcbf72b9fcb7770ef7f375
Lali98 <kisslajosbertalan77@gmail.com>
2023-03-23 14:17:03 +0100

    2023.03.23.

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import NavBar from "./NavBar";
+
+function Header() {
+    return (
+        <header className=''>
+            <h1 className='thicker text-center pt-1'>Delicious</h1>
+            <NavBar />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Components/HomePage.js
@@ -0,0 +1,14 @@
+import Header from './Header';
+import Footer from './Footer';
+
+function HomePage() {
+    return (
+        <>
+            <Header />
+            <p>Body</p>
+            <Footer />
+        </>
+    );
+}
+
+export default HomePage;
