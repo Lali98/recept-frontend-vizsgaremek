1f83f829485295270a6cd03b3270127d4ca30693
Lali98 <kisslajosbrtalan77@gmail.com>
2023-03-30 20:43:18 +0200

    api-rol érkeznek az adatok - 2023.03.30.

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,15 +1,28 @@
 import {Card, CardActionArea, CardContent, Typography} from "@mui/material";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function Categories() {
+    const [categories, setCategories] = useState([]);
+    function fetchCategories() {
+        return fetch('http://127.0.0.1:9000/api/categories')
+            .then(response => response.json())
+            .then(data => setCategories(data));
+    }
+
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
     return (
         <>
-            {[...Array(6)].map((_, i) => (
-                <div className='col-lg-6 col-md-6 col-sm-12 p-3' key={i}>
+            {categories.map((category) => (
+                <div className='col-lg-6 col-md-6 col-sm-12 p-3' key={category._id}>
                     <Card sx={{ maxWidth: 345 }}>
                         <CardActionArea href='/'>
                             <CardContent>
                                 <Typography gutterBottom variant="h6" component="div" className='text-center'>
-                                    Lizard
+                                    {category.name}
                                 </Typography>
                             </CardContent>
                         </CardActionArea>
