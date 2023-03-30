d5fb0521b62325c161cf1c6a836d34c58b26f78f
Lali98 <kisslajosbrtalan77@gmail.com>
2023-03-30 20:43:39 +0200

    2023.03.30.

--- src/Components/Upload.js
+++ src/Components/Upload.js
@@ -4,8 +4,13 @@ import { Box, TextField, Button } from "@mui/material";
 import UpgradeIcon from '@mui/icons-material/Upgrade';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { useEffect } from "react";
 
 function Upload() {
+    useEffect(() => {
+        document.title = "Delicious - Recept feltöltés";
+    }, []);
+
     return (
         <>
             <Header />
@@ -26,21 +31,34 @@ function Upload() {
                                 sx={{ width: '100%' }}
                                 name="description"
                             /><br />
-                            <div>
+                            <div className="border-bottom">
                                 <h5>Hozzávalok:</h5>
                                 {[...Array(3)].map((_, i) => (
                                     <div className="mt-3">
                                         <TextField id="outlined-basic" label={`${i + 1}. hozzávaló`} variant="outlined" sx={{ width: '80%' }} name="ingredients"/>
-                                        <button className="btn btn-success ms-1 me-1" type="button"><AddIcon /></button>
                                         <button className="btn btn-danger ms-1 me-1" type="button"><DeleteIcon /></button>
                                         <br />
                                     </div>
                                 ))}
+                                <button className="btn btn-success mb-2 mt-2" type="button"><AddIcon /></button>
                             </div>
-                            <div className="form-group row pb-3">
+                            <div>
+                                <h5>Lépések:</h5>
+                                {[...Array(5)].map((_, i) => (
+                                    <div className="mt-3">
+                                        <TextField id="outlined-basic" label={`${i + 1}. lépés`} variant="outlined" sx={{ width: '80%' }} name="ingredients"/>
+                                        <button className="btn btn-danger ms-1 me-1" type="button"><DeleteIcon /></button>
+                                        <br />
+                                    </div>
+                                ))}
+                                <button className="btn btn-success mb-2 mt-2" type="button"><AddIcon /></button>
+                            </div>
+                            <div>
                                 <h5>Kép feltöltése:</h5>
-                                <div className="col-md-6">
-                                    <input type="file" name="img-url" className="form-control" />
+                                <div className="form-group row pb-3">
+                                    <div className="col-md-6">
+                                        <input type="file" name="img-url" className="form-control" />
+                                    </div>
                                 </div>
                             </div>
                             <Button variant="contained" type="submit"><UpgradeIcon />Feltöltés</Button>
